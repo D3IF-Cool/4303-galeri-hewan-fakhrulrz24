da36a3a471fd492339ac4d4f055c78ff1d4ef034
Fakhrul_Razy <lilfawlz@student.telkomuniversity.ac.id>
2021-05-03 11:04:27 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4027/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4027/galerihewan/ui/main/MainViewModel.kt
@@ -16,36 +16,18 @@ class MainViewModel : ViewModel() {
     private val data = MutableLiveData<List<Hewan>>()
 
     init {
-        data.value = initData()
         retrieveData()
     }
 
     private fun retrieveData() {
         viewModelScope.launch {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = HewanApi.service.getHewan()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
         }
     }
 
-    // Data ini akan kita ambil dari server di langkah selanjutnya
-    private fun initData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa", "Cygnus olor","Unggas", R.drawable.angsa),
-            Hewan("Ayam", "Gallus gallus","Unggas", R.drawable.ayam),
-            Hewan("Bebek", "Cairina moschata","Unggas", R.drawable.bebek),
-            Hewan("Domba", "Ovis ammon","Mamalia", R.drawable.domba),
-            Hewan("Kalkun", "Meleagris gallopavo","Unggas", R.drawable.kalkun),
-            Hewan("Kambing", "Capricornis sumatrensis","Mamalia", R.drawable.kambing),
-            Hewan("Kelinci", "Oryctolagus cuniculus","Mamalia", R.drawable.kelinci),
-            Hewan("Kerbau", "Bubalus bubalis","Mamalia", R.drawable.kerbau),
-            Hewan("Kuda", "Equus caballus","Mamalia", R.drawable.kuda),
-            Hewan("Sapi", "Bos taurus","Mamalia", R.drawable.sapi)
-
-        )
-    }
     fun getData(): LiveData<List<Hewan>> = data
 }
